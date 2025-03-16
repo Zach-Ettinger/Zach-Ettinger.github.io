eac1d149200febe1235c925ac31cb300cd738d82
Zach-Ettinger <zachettinger999@gmail.com>
2025-03-16 18:57:23 +0000

    Improvements + background

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const splash = document.querySelector('.splash');
+const main = document.querySelector('.main');
 
 document.addEventListener('DOMContentLoaded', (e) => {
     setTimeout(() => {
         splash.classList.add('display-none');
+        main.classList.add('display-all');
     }, 2000);
 });
