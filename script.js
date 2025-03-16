551eb119d88bad8fab430e091161e0c6baa8208d
Zach-Ettinger <zachettinger999@gmail.com>
2025-03-16 19:08:08 +0000

    background fade in

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const splash = document.querySelector('.splash');
 const main = document.querySelector('.main');
+const body = document.querySelector('body');
 
 document.addEventListener('DOMContentLoaded', (e) => {
     setTimeout(() => {
         splash.classList.add('display-none');
         main.classList.add('display-all');
+        body.classList.add('display-all');
     }, 2000);
 });
