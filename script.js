ec77294cbd8722eb6516f11f30537e00ce7abb1f
Zach-Ettinger <zachettinger999@gmail.com>
2025-03-17 14:41:02 +0000

    Glowing Menu + SMOOTH

--- script.js
+++ script.js
@@ -3,19 +3,15 @@ const main = document.querySelector('.main');
 const body = document.querySelector('body');
 
 document.addEventListener('DOMContentLoaded', (e) => {
-    // Check if splash has been shown in this session
     if (!sessionStorage.getItem('splashShown')) {
-        // Show splash screen
         setTimeout(() => {
             splash.classList.add('display-none');
             main.classList.add('display-all');
             body.classList.add('display-all');
         }, 2000);
         
-        // Set the flag in sessionStorage
         sessionStorage.setItem('splashShown', 'true');
     } else {
-        // Skip splash screen if already shown in this session
         splash.classList.add('display-none');
         main.classList.add('display-all');
         body.classList.add('display-all');
